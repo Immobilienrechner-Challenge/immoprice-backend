8a30942705da29c24a94b59351360f0ecb335aca
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2022-10-12 10:06:36 +0200

    add example model request

--- model1/views.py
+++ model1/views.py
@@ -1,9 +1,26 @@
 import json
 
+from joblib import load
+from helper import ImmoHelper
 from django.http import HttpResponse
 
+helper = ImmoHelper()
+model = load('models/simple_model.joblib')
+
 def index(request):
-    return HttpResponse(f"\"request.GET\": {json.dumps(request.GET)}")
+    try:
+        data = helper.process_prediction(
+            request.GET["living_space"],
+            request.GET["type"],
+            request.GET["rooms"],
+            request.GET["gde_tax"]
+        )
+        return HttpResponse(json.dumps({
+            "prediction": model.predict(data)[0]
+        }))
+    except Exception as e:
+        return HttpResponse("Error occured, please check your input.", status = 500)
+
 
 def test(request, testnum):
     return HttpResponse(f"Model 1 test. Value: {testnum}")
