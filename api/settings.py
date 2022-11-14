319f8d88442e82042475b8a7dfd004c9e0acd926
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2022-11-14 13:55:38 +0100

    fix cors errors

--- api/settings.py
+++ api/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -51,6 +53,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "api.urls"
+
+CORS_ALLOWED_ORIGINS = [
+    "https://immoprice.ch",
+    "https://api.immoprice.ch",
+    "http://localhost:8099",
+    "http://127.0.0.1:8099",
+]
 
 TEMPLATES = [
     {
